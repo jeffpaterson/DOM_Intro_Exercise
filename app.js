350a52c1e289f8f9f54994169c8a21b00816af98
Jeff Paterson <jp@jeffpaterson.net>
2016-02-25 16:53:51 -0800

    completed first three challenges

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Javascript is alive!");
+
+var li=document.getElementsByTagName('li');
+var oImg=document.createElement("img");
+
+document.getElementById('greeting').innerText = "Hello World!";
+
+for (i=0 ; i<li.length ; i++) {
+    li[i].style.backgroundColor = "yellow";
+}
+
+// document.getElementsByTagName('li')[0].style.backgroundColor = "yellow";
+// document.getElementsByTagName('li')[1].style.backgroundColor = "yellow";
+// document.getElementsByTagName('li')[2].style.backgroundColor = "yellow";
+// document.getElementsByTagName('li')[3].style.backgroundColor = "yellow";
+// document.getElementsByTagName('li')[4].style.backgroundColor = "yellow";
+
+oImg.setAttribute('src', 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif');
+document.getElementById('greeting').appendChild(oImg);
+
